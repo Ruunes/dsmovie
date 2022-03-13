d1929bc15171ce3ecf60ce4250b8f521ac60f9ce
Ruunes <ruan0432000@gmail.com>
2022-03-12 23:09:50 -0300

    Routes

--- dsmovie/frontend/src/App.tsx
+++ dsmovie/frontend/src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import Navbar from './Components/Navbar/Index';
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
-
+import Listing from "Assets/pages/Lisiting/Index";
+import Form from "Assets/pages/Form/Index";
+import Navbar from "components/Navbar";
 
 function App() {
+
   return (
-    
-    <Navbar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieID" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
